bf23c53f83f4f00fbee5820a9502494e4e4bcd1f
Gilles Caulier <caulier.gilles@gmail.com>
2020-09-06 05:52:43 +0200

    add trnaslation rules

---
+++ i18n/es.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DigikamGenericDebianScreenshotsPlugin::DSPlugin</name>
+    <message>
+        <location filename="../src/dsplugin.cpp" line="59"/>
+        <source>DebianScreenshots</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsplugin.cpp" line="74"/>
+        <source>A tool to export to Debian Screenshots site</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsplugin.cpp" line="79"/>
+        <source>&lt;p&gt;This tool permit to export items to Debian Screenshots site.&lt;/p&gt;&lt;p&gt;See Debian Screenshots web site for details: &lt;a href=&apos;https://screenshots.debian.net/&apos;&gt;https://screenshots.debian.net/&lt;/a&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dsplugin.cpp" line="96"/>
+        <source>Export to &amp;Debian Screenshots...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DigikamGenericDebianScreenshotsPlugin::DSWidget</name>
+    <message>
+        <location filename="../src/dswidget.cpp" line="86"/>
+        <source>This is the list of images to upload to Debian Screenshots.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="93"/>
+        <source>This is a clickable link to open the Debian Screenshots home page in a web browser.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="96"/>
+        <source>Package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="97"/>
+        <source>This is the Debian Screenshots package to which selected photos will be uploaded.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="101"/>
+        <source>Package:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="125"/>
+        <source>Software version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswidget.cpp" line="134"/>
+        <source>Screenshot description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DigikamGenericDebianScreenshotsPlugin::DSWindow</name>
+    <message>
+        <location filename="../src/dswindow.cpp" line="69"/>
+        <source>Export to Debian Screenshots</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="71"/>
+        <source>Start Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="72"/>
+        <source>Start upload to Debian Screenshots web service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="170"/>
+        <source>%v / %m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="174"/>
+        <source>Debian Screenshots export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="261"/>
+        <location filename="../src/dswindow.cpp" line="279"/>
+        <source>Cannot open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dswindow.cpp" line="305"/>
+        <source>Failed to upload photo to Debian Screenshots: %1
+Do you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
