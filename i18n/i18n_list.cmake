b037fb6d9ea75a7c5e370d32ae1c7d67024a2c6b
Gilles Caulier <caulier.gilles@gmail.com>
2020-09-11 08:06:06 -0400

    add doxc on ISO639-1

--- i18n/i18n_list.cmake
+++ i18n/i18n_list.cmake
@@ -6,6 +6,11 @@
 
 # --- List of translations files to process
 
+# Append new file on list here, cmake will create missing files automatically.
+# Dont forget to add new created file on git repository.
+# Use ISO639-1 2 letter language codes as file name.
+# https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+
 set(plugin_translation_files
     ${CMAKE_SOURCE_DIR}/i18n/de.ts
     ${CMAKE_SOURCE_DIR}/i18n/es.ts
